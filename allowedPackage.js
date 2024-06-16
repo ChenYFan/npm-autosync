10a9bde23d0790295c6fcfd5569562b0a14840d9
CyanFalse <chenyf@cyfan.top>
2024-06-16 13:04:08 +0800

    UPdate

---
+++ allowedPackage.js
@@ -0,0 +1,18 @@
+export default {
+    "npm":{
+        "registry":"https://registry.npmmirror.com",
+        "packages":{
+            "cyanblog":".*",
+            "chenyfan-os":".*",
+            "chenyfan-oss":".*"
+        },
+        "scopes":{
+            "@chenyfan":{
+                "cache-db":".*",
+                //"cdnobject":".*"
+            }
+        },
+        "asyncFile":"./data/npm.json",
+        "asyncFolder":"./data/npm"
+    }
+}
---
+++ async.js
@@ -0,0 +1,118 @@
+import fetch from "node-fetch"
+import fs from "fs"
+import allowedPackage from "./allowedPackage.js"
+import { exec } from "child_process"
+
+const excuteCommand = async (command) => {
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                reject(error)
+            }
+            resolve(stdout ? stdout : stderr)
+        })
+    })
+}
+
+const loadAsyncConfig = function (AsyncFileName) {
+    this.name = AsyncFileName;
+    if (!fs.existsSync(AsyncFileName)) { fs.writeFileSync(AsyncFileName, JSON.stringify({})) }
+    this.read = () => {
+        return JSON.parse(fs.readFileSync(AsyncFileName))
+    }
+    this.write = (data) => {
+        fs.writeFileSync(AsyncFileName, JSON.stringify(data, null, 4))
+    }
+}
+
+
+
+const asyncNPM = async (config) => {
+    const { registry } = config
+    const getNPMPackageMeta = async (packageName) => {
+        return fetch(`${registry}/${packageName}`)
+            .then(response => response.json())
+    }
+    const getNPMSinglePackageMeta = async (packageName, packageVersion) => {
+        return fetch(`${registry}/${packageName}/${packageVersion}`)
+            .then(response => response.json())
+    }
+    const unTarPackage = async (FilePath, unTarFolderPath) => {
+        const command = `tar -xzf ${FilePath} -C ${unTarFolderPath}`
+        return excuteCommand(command)
+    }
+    const packageList = config.packages
+    for (let scopes in config.scopes) {
+        for (let scopePackageName in config.scopes[scopes]) {
+            packageList[scopes + "/" + scopePackageName] = config.scopes[scopes][scopePackageName]
+        }
+    }
+    for (const packageName in packageList) {
+        const npmConfig = new loadAsyncConfig(config.asyncFile)
+        const npmConfigData = await npmConfig.read()
+        // Get the package meta
+        const packageMeta = await getNPMPackageMeta(packageName)
+        console.log(packageMeta)
+        //先判断有无包
+        if (npmConfigData[packageName] === undefined) {
+            npmConfigData[packageName] = {
+                latest: null,
+                versions: []
+            }
+
+            //根据PackageName逐级创建文件夹
+            packageName.split("/").reduce((acc, cur) => {
+                acc += "/" + cur
+                if (!fs.existsSync(config.asyncFolder + "/" + acc)) {
+                    fs.mkdirSync(config.asyncFolder + "/" + acc)
+                }
+            })
+        }
+        //先判断latest
+        if (npmConfigData[packageName].latest !== packageMeta["dist-tags"].latest) {
+            npmConfigData[packageName].latest = packageMeta["dist-tags"].latest
+            const AllVersions = Object.keys(packageMeta.versions)
+            const DiffVersions = AllVersions.filter(version => !npmConfigData[packageName].versions.includes(version))
+            console.log(`Downloading ${packageName}...`)
+            for (const version of DiffVersions) {
+                console.log(`Downloading ${packageName}@${version}...`)
+                if (!version.match(new RegExp(config.packages[packageName]))) {
+                    console.log(`Skipping ${packageName}@${version}...`)
+                    continue
+                }
+                const SinglePackageMeta = await getNPMSinglePackageMeta(packageName, version)
+
+                const tarballName = SinglePackageMeta.dist.tarball.split("/").pop()
+                const tarballPath = `${config.asyncFolder}/${packageName}/${tarballName}`
+                console.log(tarballPath)
+
+
+                const tarballBuffer = await fetch(SinglePackageMeta.dist.tarball)
+                    .then(response => response.buffer())
+
+                fs.writeFileSync(tarballPath, tarballBuffer)
+                fs.mkdirSync(config.asyncFolder + "/" + packageName + "/" + version)
+                await unTarPackage(tarballPath, config.asyncFolder + "/" + packageName + "/" + version)
+                npmConfigData[packageName].versions.push(version)
+            }
+        }
+        await npmConfig.write(npmConfigData)
+    }
+
+}
+
+const asyncAll = async (config) => {
+    console.log("Updating NPM packages...")
+    await asyncNPM(config.npm)
+}
+
+!(async () => {
+    console.log("Updating packages...")
+    await asyncAll(allowedPackage)
+        .then(() => {
+            console.log("Downloaded all packages...")
+        })
+        .catch((err) => {
+            console.error(err)
+        })
+})()
