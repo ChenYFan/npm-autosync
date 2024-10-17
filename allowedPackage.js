2b61a80acd6e1f06743585fb2957f959e21ce912
CyanFalse <chenyf@cyfan.top>
2024-10-17 22:08:20 +0800

    fix

--- allowedPackage.js
+++ allowedPackage.js
@@ -2,9 +2,9 @@ export default {
     "npm":{
         "registry":"https://registry.npmjs.org",
         "packages":{
-            "cyanblog":".*",
-            "chenyfan-os":".*",
-            "chenyfan-oss":".*",
+            // "cyanblog":".*",
+            // "chenyfan-os":".*",
+            // "chenyfan-oss":".*",
             "pagemap":"1.4.0",
             "notyf":"3.10.0",
             "artalk":"2.8.2",
